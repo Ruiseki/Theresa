50a14ad649e883fe9a6947ace9c5d4a2957ec4fb
Ruiseki <jojobru60@gmail.com>
2023-12-11 10:55:54 +0100

    api -> sending music json with login

--- api/user.js
+++ api/user.js
@@ -9,14 +9,15 @@ export async function init()
 async function login(req, res)
 {
     let index = await isUserExist(req.body.username, req.body.password);
-    if(index == -1) res.sendStatus(400);
+    if(index == -1) res.sendStatus(404);
     else
     {
         res.status(200).json(
             {
                 username: usersCache[index].username,
                 password: usersCache[index].password,
-                discordId :usersCache[index].discordId,
+                discordId:usersCache[index].discordId,
+                musics:   usersCache[index].musics,
             }
         );
     }
